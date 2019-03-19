a09db6d0f1a4cfbb77dcf41eca6d1790972309f6
James McKinnon <james.mckinnon@wildfireinternet.co.uk>
2019-03-19 16:51:42 +0000

    feat: Completed inital design for mobile

--- js/modules/contact-form.js
+++
@@ -1,7 +0,0 @@
-
-jQuery(function ($) {
-	if ($('#honeypot_hybrid_answer_772').length > 0) {
-		$("#honeypot_hybrid_answer_772").val("9");
-	    $("#rec_honeypot_hybrid_772").css("display", "none");
-	}
-});
